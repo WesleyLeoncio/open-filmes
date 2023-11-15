5f80e5bdda0c3b46bf441632219a5522c2dca1bc
Wesley <wesley.leoncio.izi@gmail.com>
2023-11-15 16:48:47 -0300

    Atualização na pagina inicial usando rota

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../service/user.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -9,11 +10,13 @@ import { UserService } from "../../service/user.service";
 export class HeaderComponent implements OnInit{
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) {
   }
 
   ngOnInit(): void {
+      this.router.navigate(['/filmes'])
       console.log(this.userService.getUserLoginInfor());
   }
 
